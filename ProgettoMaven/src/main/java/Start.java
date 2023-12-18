01be622383c959fa1e5af430db821f5c8d21ada3
luigi.tognoni <luigi.tognoni.majo@gmail.com>
2023-12-18 11:07:04 +0100

    prova errore

--- ProgettoMaven/src/main/java/Start.java
+++ ProgettoMaven/src/main/java/Start.java
@@ -3,7 +3,7 @@ public class Start {
 
 	public static void main(String[] args)
 	{
-		System.out.println("ciao mondo real maveen a");
+		System.out.printn("ciao mondo real maveen a");
 		System.out.println("ciao mondo real maveen a");
 		System.out.println("ciao mondo real maveen a");
 		System.out.println("ciao mondo real maveen a");
